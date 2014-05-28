6ccbb4984e874835c5bf4977c734abacd3e0009d
Charles Butler <chuck@dasroot.net>
2014-05-28 12:49:20 -0400

    Updates to field names to reflect class updates and parser expectations

--- contrib/bind/provider.py
+++ contrib/bind/provider.py
@@ -31,7 +31,8 @@ class BindProvider(object):
         zp = ZoneParser(domain)
         # Install a skeleton bind zone, rehashes existing file
         # if it has contents)
-        self.first_setup(zp, domain)
+        if not os.path.exists('/etc/bind/db.%s' % domain):
+            self.first_setup(zp, domain)
         zp.save()
 
     def add_record(self, domain='example.com', record={}):
@@ -45,14 +46,16 @@ class BindProvider(object):
         # Insert SOA and NS records
         addr = unit_get('public-address')
         parser.dict_to_zone({'rr': 'SOA',
-                             'addr': '@',
-                             'alias': 'root.%s' % domain,
+                             'addr': 'ns.%s.' % domain,
+                             'owner': 'root.%s.' % domain,
                              'serial': '2003080800',
                              'refresh': '12h',
                              'update-retry': '15m',
                              'expiry': '3w',
                              'minimum': '3h'})
-        parser.dict_to_zone({'rr': 'NS', 'owner-name': '@',
+        parser.dict_to_zone({'rr': 'NS', 'alias': '@',
                              'addr': 'ns1.%s.' % domain})
-        parser.dict_to_zone({'rr': 'A', 'alias': '@', 'addr': addr})
-        parser.dict_to_zone({'rr': 'A', 'alias': 'ns1', 'addr': addr})
+        parser.dict_to_zone({'rr': 'A', 'alias': '@', 'addr': addr,
+                             'ttl': 300})
+        parser.dict_to_zone({'rr': 'A', 'alias': 'ns1', 'addr': addr,
+                             'ttl': 300})
