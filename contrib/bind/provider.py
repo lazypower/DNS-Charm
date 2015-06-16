91e42ec3f921fa021e55a41562d8a3acf260b561
Charles Butler <chuck@dasroot.net>
2015-06-15 23:29:04 -0400

    Adjust bind provider config-changed logic to store self.domain

--- contrib/bind/provider.py
+++ contrib/bind/provider.py
@@ -10,17 +10,20 @@ from common import resolve_hostname_to_ip
 
 class Provider(object):
 
-    def config_changed(self, domain='example.com'):
-        zp = ZoneParser(domain)
+    def __init__(self, domain):
+        self.domain = domain
+
+    def config_changed(self):
+        zp = ZoneParser(self.domain)
         # Install a skeleton bind zone, rehashes existing file
         # if it has contents)
-        if not os.path.exists('/etc/bind/db.%s' % domain):
-            self.first_setup(zp, domain)
+        if not os.path.exists('/etc/bind/db.%s' % self.domain):
+            self.first_setup(zp)
             zp.save()
             self.reload_config()
 
-    def add_record(self, record, domain='example.com'):
-        zp = ZoneParser(domain)
+    def add_record(self, record):
+        zp = ZoneParser(self.domain)
         if type(record) is dict:
             zp.dict_to_zone(record)
         elif type(record) is list:
@@ -30,26 +33,26 @@ class Provider(object):
         zp.save()
         self.reload_config()
 
-    def remove_record(self, record, domain='example.com'):
-        zp = ZoneParser(domain)
+    def remove_record(self, record):
+        zp = ZoneParser(self.domain)
         zp.zone.remove('alias', record['rr'], record['alias'])
         zp.save()
         self.reload_config()
 
-    def first_setup(self, parser, domain='example.com'):
+    def first_setup(self, parser):
         # Insert SOA and NS records
         hostname = unit_get('public-address')
         addr = resolve_hostname_to_ip(hostname)
         parser.dict_to_zone({'rr': 'SOA',
-                             'addr': 'ns.%s.' % domain,
-                             'owner': 'root.%s.' % domain,
+                             'addr': 'ns.%s.' % self.domain,
+                             'owner': 'root.%s.' % self.domain,
                              'serial': randint(12345678, 22345678),
                              'refresh': '12h',
                              'update-retry': '15m',
                              'expiry': '3w',
                              'minimum': '3h'})
         parser.dict_to_zone({'rr': 'NS', 'alias': '@', 'ttl': 1600,
-                             'addr': 'ns.%s.' % domain})
+                             'addr': 'ns.%s.' % self.domain})
         parser.dict_to_zone({'rr': 'A', 'alias': 'ns', 'addr': addr,
                              'ttl': 300})
 
