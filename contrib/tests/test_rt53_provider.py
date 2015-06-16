516ca4c86981db9f28f3ee0379e3a1645fd92a69
Charles Butler <chuck@dasroot.net>
2015-06-16 00:53:38 -0400

    Test refactoring to reflect latest updates

--- contrib/tests/test_rt53_provider.py
+++ contrib/tests/test_rt53_provider.py
@@ -3,7 +3,6 @@ import pytest
 import os
 
 from rt53 import provider
-#import rt53
 
 class TestRt53Provider():
     """
@@ -11,7 +10,7 @@ class TestRt53Provider():
     anything. But it's fair to assume that you want actual integration tests
     and you should probably stuff in real credentials then.
 
-    export AWS_ACCESS_KEY
+    export AWS_ACCESS_KEY_ID
     export AWS_SECRET_ACCESS_KEY
 
     in your CI environment to ensure these tests dont fail due to cloud creds
@@ -20,33 +19,17 @@ class TestRt53Provider():
 
     @classmethod
     def setup_class(cls):
-        AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+        AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
         cls.provider = provider.Provider('example.com', AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
 
 
-    def test_zoneid_from_domain(self):
-        """
-            this is an integration level test until I figure out what to mock
-            in the route53 lib
-        """
-        id = self.provider.get_zone_id('example.com')
-        if not os.getenv('RT53DOMAIN_KEY'):
-            assert id == "ZD4S1IEWT6LPB"
-        else:
-            assert id == os.getenv('RT53DOMAIN_KEY')
-
-
-    def test_config_changed(self):
-        with patch('rt53.provider.Provider.get_zone_id') as get:
-            self.provider.config_changed()
-            get.assert_called_with('example.com')
-
-    def test_add_record_with_dict(self):
+    @patch('rt53.provider.route53.record.ResourceRecordSets')
+    def test_add_record_with_dict(self, rrm):
         record = {'name': 'test', 'rr': 'A', 'ttl': 1600}
-        with patch('rt53.provider.Provider.dict_entry') as de:
+        with patch('rt53.provider.Provider.parse_record') as de:
             self.provider.add_record(record)
-            de.assert_called_with(record)
+            de.assert_called_once(record, rrm)
 
     @patch('rt53.provider.Provider.create_a_record')
     @patch('rt53.provider.Provider.create_cname_record')
@@ -65,10 +48,11 @@ class TestRt53Provider():
 
 
     def test_create_a_record(self):
-        record = {'name': 'test', 'ttl': 1600, 'rr': 'A', 'addr': '127.0.0.1'}
-        with patch('route53.hosted_zone.HostedZone.create_a_record') as crm:
-            self.provider.create_a_record(record)
-            crm.assert_called_with('test', '127.0.0.1', 1600)
+        record = {'alias': 'test', 'ttl': 1600, 'rr': 'A', 'addr': '127.0.0.1'}
+        with patch('boto.route53.record.ResourceRecordSets') as crm:
+            self.provider.create_a_record(record, crm)
+            crm.add_change.assert_called_with('UPSERT', 'test.example.com',
+                                              'A', ttl=1600)
 
     def test_create_cname_record(self):
         pass
